732b12abf55f103c15131c466dd6bf1eb0e1d59a
Halil Tuncay Uner <tuncayuner@gmail.com>
2018-10-09 16:29:28 +0300

    expert break rule added

---
+++ src/WorkoutProgram/Rule/ConcreteRule/ExpertsShouldGetTwoMinuteBreaks.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OneFit\WorkoutProgram\Rule\ConcreteRule;
+
+use DateInterval;
+use OneFit\Activity\ActivityInterface;
+use OneFit\Activity\ActivityType;
+use OneFit\DomainUtilsTrait;
+use OneFit\Person\PersonLevel;
+use OneFit\WorkoutProgram\Rule\RuleInterface;
+use OneFit\WorkoutProgram\WorkoutProgram;
+
+/**
+ * Class ExpertsShouldGetTwoMinuteBreaks
+ * @package OneFit\WorkoutProgram\Rule\ConcreteRule
+ * @author Halil Tuncay Üner <tuncayuner@gmail.com>
+ */
+class ExpertsShouldGetTwoMinuteBreaks implements RuleInterface
+{
+    public function resolve(WorkoutProgram $workoutProgram, ActivityInterface $activity, DateInterval $duration): bool
+    {
+        $participantLevel = $workoutProgram->getParticipant()->getLevel();
+
+        $participantLevelMatches = PersonLevel::EXPERT === $participantLevel;
+
+        $activityTypeMatches = $activity->getType() === ActivityType::TYPE_BREAK;
+
+        $durationMatches = DomainUtilsTrait::dateIntervalToSeconds($duration) ===
+            DomainUtilsTrait::dateIntervalToSeconds(DateInterval::createFromDateString('2 minutes'));
+
+        if ($participantLevelMatches && $activityTypeMatches && $durationMatches === false) {
+            throw new \LogicException('Experts should take 2 minute only breaks.');
+        }
+
+        return true;
+    }
+}
---
+++ test/WorkoutProgram/Rule/ConcreteRule/ExpertsShouldGetTwoMinuteBreaksTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OneFit\Test\WorkoutProgram\Rule\ConcreteRule;
+
+use DateInterval;
+use OneFit\Activity\ConcreteActivity\BreakActivity;
+use OneFit\Person\Person;
+use OneFit\Person\PersonLevel;
+use OneFit\Test\WorkoutProgram\WorkoutProgramTest;
+use OneFit\WorkoutProgram\Rule\ConcreteRule\ExpertsShouldGetTwoMinuteBreaks;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ExpertsShouldGetTwoMinuteBreaksTest
+ * @package OneFit\Test\WorkoutProgram\Rule\ConcreteRule
+ * @author Halil Tuncay Üner <tuncayuner@gmail.com>
+ */
+class ExpertsShouldGetTwoMinuteBreaksTest extends TestCase
+{
+    public function testExpertGetsThreeMinuteBreak(): void
+    {
+        $this->expectException('LogicException');
+
+        $beginner = new Person('Foo Bar', PersonLevel::EXPERT);
+
+        $workoutProgram = WorkoutProgramTest::createWorkoutProgram($beginner);
+
+        $rule = new ExpertsShouldGetTwoMinuteBreaks();
+
+        $breakActivity = new BreakActivity();
+
+        $rule->resolve($workoutProgram, $breakActivity, DateInterval::createFromDateString('3 minutes'));
+    }
+
+    public function testExpertGetsTwoMinuteBreak(): void
+    {
+        $beginner = new Person('Foo Bar', PersonLevel::EXPERT);
+
+        $workoutProgram = WorkoutProgramTest::createWorkoutProgram($beginner);
+
+        $rule = new ExpertsShouldGetTwoMinuteBreaks();
+
+        $breakActivity = new BreakActivity();
+
+        $resolved = $rule->resolve($workoutProgram, $breakActivity, DateInterval::createFromDateString('2 minutes'));
+
+        $this->assertTrue($resolved);
+    }
+}
